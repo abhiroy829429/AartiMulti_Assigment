5eebddf0141f9b3858f10452c9c0382cb7313ad6
Abhimanyu Kumar <abhiroy829429@gmail.com>
2025-07-14 12:17:32 +0530

    checking environment

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
 
+const authRoutes = require('./routes/auth');
+const questionRoutes = require('./routes/questions');
+const scoreRoutes = require('./routes/scores');
+
 const PORT = process.env.PORT || 5001;
 // const __dirname = path.resolve();
 const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://abhiroy829429:QD3fYCOSxlAfUrlx@cluster0.h57dczo.mongodb.net/quizapp?retryWrites=true&w=majority&appName=Cluster0';
@@ -23,14 +27,11 @@ app.use(cors({
 
 // });
 
-const authRoutes = require('./routes/auth');
-const questionRoutes = require('./routes/questions');
-const scoreRoutes = require('./routes/scores');
 
 app.use('/api/auth', authRoutes);
 app.use('/api', questionRoutes);
 app.use('/api', scoreRoutes);
-
+console.log(process.env.NODE_ENV);
 if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -39,12 +40,10 @@ if(process.env.NODE_ENV === "production"){
   });
 }
 
-
-mongoose.connect(MONGO_URI)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 }); 
