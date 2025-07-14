81b22f8a5c12a19686ec1cee434082f153e836b9
Abhimanyu Kumar <abhiroy829429@gmail.com>
2025-07-14 13:27:50 +0530

    db connection

--- backend/index.js
+++ backend/index.js
@@ -33,10 +33,7 @@ if(process.env.NODE_ENV === "production"){
   });
 }
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  mongoose.connect(MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB connection error:', err));
-
-}); 
+mongoose.connect(MONGO_URI).then(() =>{
+  console.log('MongoDB connected');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
