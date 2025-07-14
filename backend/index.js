4d715003f1cc36a47fb88817d05f8220ba03c547
Abhimanyu Kumar <abhiroy829429@gmail.com>
2025-07-14 12:35:08 +0530

    fix environment veriable

--- backend/index.js
+++ backend/index.js
@@ -32,13 +32,13 @@ app.use('/api/auth', authRoutes);
 app.use('/api', questionRoutes);
 app.use('/api', scoreRoutes);
 console.log(process.env.NODE_ENV);
-// if(process.env.NODE_ENV === "production"){
+if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
-// }
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
